fcc58f9987e3ad8530e1ec4a47b46ab36c9ed461
= <tw7613781@gmail.com>
2018-11-24 02:02:29 +0900

    chore: add a dependency at setup

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/tw7613781/ssdp_upnp",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'colorlog>=3.1.4'
+    ]
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
